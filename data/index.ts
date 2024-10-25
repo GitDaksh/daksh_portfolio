83ffc81ea5962a45e6d137d92beeb9a3a2747370
Daksh <daksh.java.util@gmail.com>
2024-10-25 18:54:44 +0530

    update projects

---
+++ data/index.ts
@@ -0,0 +1,169 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building a Video Conferencing Web App",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Wanderlust - Travel Site",
+      des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+      img: "/p1.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+      link: "https://github.com/GitDaksh",
+    },
+    {
+      id: 2,
+      title: "VisualVibe - Video Conferencing App",
+      des: "Simplify your video conferencing experience with VisualVibe. Seamlessly connect with colleagues and friends.",
+      img: "/p2.jpg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+      link: "https://visualvibe-green.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "LinkUp - A Social Networking Site",
+      des: "A professional networking platform built with Next.js, designed to connect like-minded individuals and foster career growth. Inspired by LinkedIn.",
+      img: "/p3.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+      link: "https://github.com/GitDaksh",
+    },
+    {
+      id: 4,
+      title: "C Compiler",
+      des: "A robust C compiler built to convert pre-processed C files into efficient assembly code. This compiler performs lexical analysis, syntax and semantic checks, optimization, and code generation.",
+      img: "/p4.svg",
+      iconLists: [],
+      link: "https://github.com/GitDaksh",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "FullStack Development Intern @ Cloud Counselage Pvt. Ltd.",
+      desc: "Performing several tasks including training numerous AI models and completed several Programming missions",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "AI Training Coding Expert @ OutlierAI",
+      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+      className: "md:col-span-2", 
+      thumbnail: "/exp2.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+    },
+  ];
